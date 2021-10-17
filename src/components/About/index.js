662b3481d756c43fdb5d3a1cfe8d7d284099b8c9
Julissa Hash <69886471+yoyachi@users.noreply.github.com>
2021-10-17 16:47:16 -0500

    Updated About

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,7 +9,7 @@ function About() {
       <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
       <div className="text">
         <p>
-          I am a full stack developer instructed by the University of Texas coding bootcamp program. This program helped me setting knowledge on newly development skills in JavaScript, CSS, React.js and responsive web design MERN. With this knowledge I am looking to transform and define current/new applications, to create a more effective and dynamic user interaction. I am looking forward to join a developer company that would appreciate innovation, interactivity and desirable web design which is one of my biggest passions.  Altogether with this employer, I would look to setup better solutions to a competitive and rapid changing developer world.
+          I am a full stack developer instructed by the University of Texas coding boot camp. This program helped me setting knowledge on newly development skills in JavaScript, CSS, React.js and responsive web design MERN. With this knowledge I am looking forward to update current applications, and create new ones developing a more effective and dynamic user interaction. I am looking forward to join a developer company that would appreciate innovation, interactivity and desirable web design which is one of my biggest passions.  Altogether with this employer, I would look forward to setup better solutions for a competitive and rapid changing developer world.
       </p>
       </div>
     </section>
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 function Resume() {
 	return (
 		<section className= "resume-section">
