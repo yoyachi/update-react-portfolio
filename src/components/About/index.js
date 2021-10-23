b9dcb1ca31d75e9f40baa446cc871fa99a9446a6
Julissa Hash <69886471+yoyachi@users.noreply.github.com>
2021-10-23 11:42:08 -0500

    Updated card-Portfolio

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import coverImage from "../../assets/cover/IMG_8236.JPG";
-
+import backgroundImage from "../../assets/cover/austin.jpg";
 function About() {
 
   return (
     <section className="my-5" >
       <div className="card shadow border-0">
-        
-      <div>
-      <h1 id="about">Julissa Hash</h1>
-      <p>Software Developer</p>
-      <p>Austin, Texas</p>
-      </div>
 
-      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+        <div>
+          <h1 id="about">Julissa Hash</h1>
+          <p>Software Developer</p>
+          <p>Austin, Texas</p>
+        </div>
+
+        <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      
       </div>
       <div className="text">
         <p>
           I am a full stack developer instructed by the University of Texas coding boot camp. This program helped me setting knowledge on newly development skills in JavaScript, CSS, React.js and responsive web design MERN. With this knowledge I am looking forward to update current applications, and create new ones developing a more effective and dynamic user interaction. I am looking forward to join a developer company that would appreciate innovation, interactivity and desirable web design which is one of my biggest passions.  Altogether with this employer, I would look forward to setup better solutions for a competitive and rapid changing developer world.
-      </p>
+        </p>
       </div>
     </section>
   );
