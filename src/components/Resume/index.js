82ad7d8fe3037898e5e9f8e6c6f1cce563f687b5
Julissa Hash <69886471+yoyachi@users.noreply.github.com>
2021-11-02 18:36:15 -0500

    Updated resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -7,7 +7,7 @@ function Resume() {
 				<h1 className="page-header">My Resume</h1>
 			</div>
 			<div className="bottom-spacing">
-				<a href={"/files/Julissa's-Resume.pdf"} download>
+				<a href={"/files/Julissa's-Resume (6).pdf"} download>
 					<h4>Download my Resume</h4>
 				</a>
 			</div>
